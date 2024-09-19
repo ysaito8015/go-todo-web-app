35828031fd935c02a2bb7f9123a01a512a7cdb4d
Yusuke_Saito <info@ysaito.dev>
2024-09-19 19:41:38 +0900

    section-061

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"golang.org/x/sync/errgroup"
 
@@ -21,6 +24,8 @@ func main() {
 }
 
 func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -33,6 +38,7 @@ func run(ctx context.Context) error {
 	log.Printf("start with: %v", url)
 	s := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(10 * time.Second)
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
