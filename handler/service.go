2d1b024d7958fe43baebaa29e06b028a8264f533
Yusuke_Saito <info@ysaito.dev>
2024-09-21 23:34:49 +0900

    section-076

---
+++ handler/service.go
@@ -0,0 +1,16 @@
+package handler
+
+import (
+	"context"
+
+	"github.com/ysaito8015/go-todo-web-app/entity"
+)
+
+//go:generate go run github.com/matryer/moq -out moq_test.go . ListTasksService AddTaskService
+type ListTasksService interface {
+	ListTasks(ctx context.Context) (entity.Tasks, error)
+}
+
+type AddTaskService interface {
+	AddTask(ctx context.Context, title string) (*entity.Task, error)
+}
