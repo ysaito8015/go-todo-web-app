b7ce40d99d4054a66fc7066d339667bd340a0f76
Yusuke_Saito <info@ysaito.dev>
2024-09-22 22:35:35 +0900

    section-079

--- handler/service.go
+++ handler/service.go
@@ -6,11 +6,15 @@ import (
 	"github.com/ysaito8015/go-todo-web-app/entity"
 )
 
-//go:generate go run github.com/matryer/moq -out moq_test.go . ListTasksService AddTaskService
+//go:generate go run github.com/matryer/moq -out moq_test.go . ListTasksService AddTaskService RegisterUserService
 type ListTasksService interface {
 	ListTasks(ctx context.Context) (entity.Tasks, error)
 }
 
 type AddTaskService interface {
 	AddTask(ctx context.Context, title string) (*entity.Task, error)
+}
+
+type RegisterUserService interface {
+	RegisterUser(ctx context.Context, name, password, role string) (*entity.User, error)
 }
--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
 
 	"github.com/ysaito8015/go-todo-web-app/config"
 )
@@ -22,12 +26,38 @@ func run(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+
+	c := mysql.Config{
+		User:      cfg.DBUser,
+		Passwd:    cfg.DBPassword,
+		Addr:      cfg.DBHost + ":" + string(rune(cfg.DBPort)),
+		DBName:    cfg.DBName,
+		ParseTime: true,
+	}
+
+	db, err := sql.Open("mysql", c.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// 稼働待ち
+	for {
+		err = db.Ping()
+		if err == nil {
+			break
+		}
+		time.Sleep(3 * time.Second)
+	}
+	log.Println("DB is ready")
+
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
 	if err != nil {
 		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
+
 	mux, cleanup, err := NewMux(ctx, cfg)
 	defer cleanup()
 	if err != nil {
